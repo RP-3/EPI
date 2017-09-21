084bb912868dad0dc99dbeadb5da7338d789865c
Rohan Pethiyagoda <sarith21@gmail.com>
2017-09-21 07:01:06 -0700

    Add basic test case for dudupeSortedArray in c

---
+++ c/arrays/dedupeSortedArray/implementation.c
@@ -0,0 +1,69 @@
+/*
+Write a function that takes a sorted array of integers and removes duplicate
+entries from it. It should return the number of elements removed. 
+*/
+
+/*
+* Begin test setup
+*/
+#include <stdio.h>
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define RESET "\x1B[0m"
+
+void test(char assertion[], int (*func)(int[],  int), int a[], int size, int expectation[]);
+void print_int_array(int source[], int size);
+int int_array_equal(int a[], int b[], int size);
+
+void print_int_array(int source[], int size){
+    printf("[");
+    for(int i=0; i<size; i++)
+        printf(" %d ", source[i]);
+    printf("]\n");
+}
+
+int int_array_equal(int a[], int b[], int size){
+    for(int i=0; i<size; i++)
+        if(a[i] != b[i])
+            return 0;
+
+    return 1;
+}
+
+
+void test(char assertion[], int (*func)(int[],  int), int a[], int size, int expectation[]){
+
+    func(a, size);
+
+    if(int_array_equal(a, expectation, size)){
+        printf("%s %s \u2713 %s\n", assertion, KGRN, RESET);
+    }else{
+        printf("%s %s \u2717 %s\n", assertion, KRED, RESET);
+        printf("    Expected :  "); print_int_array(expectation, size);
+        printf("    But got  :  "); print_int_array(a, size);
+    }
+}
+
+/*
+* End test setup and begin implementation
+*/
+
+#include <stdlib.h>
+
+// we could use shorts here instead of ints... 
+int deduplicate_int_array(int a[], int size);
+int deduplicate_int_array(int a[], int size){
+
+    return 0;
+}
+
+
+int main(){
+
+    int t1a1[9] = {2, 3, 5, 5, 7, 11, 11, 11, 13};
+    int t1expect[9] = {2, 3, 5, 7, 11, 13, 0, 0, 0};
+
+    test("0 * 0 should equal 0", deduplicate_int_array, t1a1, 9, t1expect);
+
+    return 0;
+}
